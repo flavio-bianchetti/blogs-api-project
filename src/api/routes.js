5189ce1dadc71ac20d085ea9b4cd0cfa8fee09b6
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-13 17:11:06 -0300

    feat: routes.js - categoryController route included

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,7 +1,9 @@
 const userController = require('../controllers/userController');
 const loginController = require('../controllers/loginController');
+const categoryController = require('../controllers/categoryController');
 
 module.exports = {
   userController,
   loginController,
+  categoryController,
 };
