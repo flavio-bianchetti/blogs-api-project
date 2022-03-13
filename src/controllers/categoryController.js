7512b6c20505062781dff2ce1a9348001219262d
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-13 17:09:29 -0300

    feat: categoryController.js - POST /categories included

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const CategoryService = require('../services/categoryService');
+const validateJWTMiddleware = require('../middlewares/validateJWTMiddleware');
+
+const router = express.Router();
+
+router.post('/', validateJWTMiddleware, async (req, res) => {
+  try {
+    const { name } = req.body;
+    const result = await CategoryService.create({ name });
+
+    if (result.error) return res.status(400).json({ error: result.error });
+
+    if (result.message) return res.status(result.status).json({ message: result.message });
+
+    return res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error });
+  }
+});
+
+module.exports = router;
