822e62cd4004f7a9c2e04e0d0fa635bbfc5f218d
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-13 17:22:07 -0300

    feat: categoryService.js - GetAll method included

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -19,6 +19,17 @@ const create = async ({ name }) => {
   }
 };
 
+const getAll = async () => {
+  try {
+    const category = await Category.findAll();
+    return category;
+  } catch (err) {
+    console.error(err);
+    return { error: err.message };
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
