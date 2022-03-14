fb66fd10e1a556950c0f35a38217fd049092af74
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-14 15:00:24 -0300

    feat: routes.js - postController route included

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,9 +1,11 @@
 const userController = require('../controllers/userController');
 const loginController = require('../controllers/loginController');
 const categoryController = require('../controllers/categoryController');
+const postController = require('../controllers/postController');
 
 module.exports = {
   userController,
   loginController,
   categoryController,
+  postController,
 };
