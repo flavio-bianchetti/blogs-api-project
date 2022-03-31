e8fa5e2bee0d34b70f97c8eb49ca15d76e25906e
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-31 13:58:12 -0300

    fix: postService.js - removed commented code

--- src/services/postService.js
+++ src/services/postService.js
@@ -14,28 +14,6 @@ const findOneResult = async (id) => {
    return result;
 };
 
-// const create = async ({ title, content, userId, categoryIds }) => {
-//   const t = await sequelize.transaction();
-//   try {
-//     const p = await BlogPost
-//       .create({ title, content, userId, published: new Date(), updated: new Date() },
-//         { transaction: t });
-
-//     return Promise.all(categoryIds.map((c) => PostCategory.create({ postId: p.id, categoryId: c },
-//       { transaction: t }))).then(() => {
-//           t.commit();
-//           return p.dataValues; 
-//       }).catch(() => {
-//           t.rollback();
-//           return { error: 'Error creating post' }; 
-//       });
-//   } catch (err) {
-//     await t.roolback();
-//     console.error(err);
-//     return { error: err.message };
-//   }
-// };
-
 const create = async ({ title, content, userId, categoryIds }) => {
   const t = await sequelize.transaction();
   return BlogPost.create({ title, content, userId, published: new Date(), updated: new Date() },
